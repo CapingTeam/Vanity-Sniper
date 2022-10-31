a772c7d739d6a1a4a9dfeb3556da74bbe67e363a
Sans <99873347+002-sans@users.noreply.github.com>
2022-10-31 22:35:53 +0100

    Vanity Sniper 1.0.0

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "s", // Place your token here / Bot token
+    guildid: "", // Put the ID of the guild here
+    url: "" // place the URL (only after the last / like: discord.gg/ -->> aigis)
+}
